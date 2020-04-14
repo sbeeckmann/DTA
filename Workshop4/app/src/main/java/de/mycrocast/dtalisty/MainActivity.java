2ec77266ac3eb61e831dfb43c4908713f8c1b577
Sven Beeckmann <sven.beeckmann@ifak.eu>
2020-04-14 22:21:23 +0200

    workshop 4 implementation done updated some stuff for different other workshops

---
+++ Workshop4/app/src/main/java/de/mycrocast/dtalisty/MainActivity.java
@@ -0,0 +1,182 @@
+package de.mycrocast.dtalisty;
+
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Random;
+
+import de.mycrocast.dtalisty.adapter.EntryAdapter;
+import de.mycrocast.dtalisty.data.Entry;
+
+public class MainActivity extends AppCompatActivity implements EntryAdapter.ClickListener {
+
+    private RecyclerView entryRecyclerView;
+    private EntryAdapter entryAdapter;
+
+    private List<Entry> entryData;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        this.entryRecyclerView = this.findViewById(R.id.entries);
+
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        this.entryRecyclerView.setLayoutManager(linearLayoutManager);
+
+        this.entryData = this.createDummyData();
+        this.entryAdapter = new EntryAdapter(this.entryData);
+        this.entryAdapter.setClickListener(this);
+
+        this.entryRecyclerView.setAdapter(entryAdapter);
+
+        this.sortEntries();
+    }
+
+    private void sortEntries() {
+        Collections.sort(this.entryData, new Comparator<Entry>() {
+            @Override
+            public int compare(Entry o1, Entry o2) {
+                switch (o1.getPriority()) {
+                    case HIGH: {
+                        if (!o1.isActive() && !o2.isActive()) {
+                            if (o2.getPriority() != Entry.Priority.HIGH) {
+                                return -1;
+                            }
+                            return 0;
+                        }
+                        if (o1.isActive() && !o2.isActive()) {
+                            return -1;
+                        }
+
+                        if (!o1.isActive() && o2.isActive()) {
+                            return 1;
+                        }
+                        if (o1.isActive() && o2.isActive()) {
+                            if (o2.getPriority() == Entry.Priority.HIGH) {
+                                return 0;
+                            }
+                            return -1;
+                        }
+                        break;
+                    }
+                    case NORMAL: {
+                        if (!o1.isActive() && !o2.isActive()) {
+                            if (o2.getPriority() == Entry.Priority.NORMAL) {
+                                return 0;
+                            }
+                            if (o2.getPriority() == Entry.Priority.HIGH) {
+                                return 1;
+                            }
+                            return -1;
+                        }
+                        if (o1.isActive() && !o2.isActive()) {
+                            return -1;
+                        }
+                        if (!o1.isActive() && o2.isActive()) {
+                            return 1;
+                        }
+                        if (o1.isActive() && o2.isActive()) {
+                            if (o2.getPriority() == Entry.Priority.HIGH) {
+                                return 1;
+                            }
+                            if (o2.getPriority() == Entry.Priority.LOW) {
+                                return -1;
+                            }
+                            return 0;
+                        }
+                        break;
+                    }
+                    case LOW: {
+                        if (!o1.isActive() && !o2.isActive()) {
+                            if (o2.getPriority() == Entry.Priority.LOW) {
+                                return 0;
+                            }
+                            return 1;
+                        }
+                        if (o1.isActive() && !o2.isActive()) {
+                            return -1;
+                        }
+                        if (!o1.isActive() && o2.isActive()) {
+                            return 1;
+                        }
+                        if (o1.isActive() && o2.isActive()) {
+                            if (o2.getPriority() == Entry.Priority.LOW) {
+                                return 0;
+                            }
+                            return 1;
+                        }
+                        break;
+                    }
+                    default: {
+                        return 0;
+                    }
+                }
+                return 0;
+            }
+        });
+    }
+
+
+    private List<Entry> createDummyData() {
+        List<Entry> dummies = new ArrayList<>();
+
+        // build in some randomness
+        Random random = new Random();
+        Entry.Priority[] priorities = Entry.Priority.values();
+        String[] todoTasks = new String[]{"Android lernen", "DTA Workshop toll finden", "Konkurrenzanalyse", "IOS lernen"};
+
+        for (int i = 0; i < 10; i++) {
+            boolean entryActive = random.nextInt(2) == 0;
+            String todoTask = todoTasks[random.nextInt(todoTasks.length)];
+            Entry.Priority priority = priorities[random.nextInt(priorities.length)];
+
+            Entry entry = new Entry(todoTask);
+            entry.setCreationTime(System.currentTimeMillis() - (1000 * 60 * 60 * 24 * i)); // we subtract i days of the current time in milliseconds
+            entry.setActive(entryActive);
+            entry.setPriority(priority);
+
+            dummies.add(entry);
+        }
+
+        return dummies;
+    }
+
+    @Override
+    public void onEntryClick(View view, int entryIndex) {
+        final Entry entry = this.entryData.get(entryIndex);
+
+        entry.setActive(!entry.isActive());
+        this.sortEntries();
+
+        this.entryAdapter.notifyDataSetChanged();
+    }
+
+    @Override
+    public void onDeleteClick(View view, final int entryIndex) {
+        final Entry entry = this.entryData.get(entryIndex);
+
+        new AlertDialog.Builder(MainActivity.this)
+                .setMessage("Eintrag: " + entry.getName() + " löschen?")
+                .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        entryData.remove(entry);
+                        entryAdapter.notifyItemRemoved(entryIndex);
+                    }
+                })
+                .setNegativeButton(android.R.string.no, null)
+                .show();
+    }
+}
---
+++ Workshop4/app/src/main/java/de/mycrocast/dtalisty/adapter/EntryAdapter.java
@@ -0,0 +1,143 @@
+package de.mycrocast.dtalisty.adapter;
+
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+
+import de.mycrocast.dtalisty.R;
+import de.mycrocast.dtalisty.data.Entry;
+
+public class EntryAdapter extends RecyclerView.Adapter<EntryAdapter.EntryViewHolder> {
+
+    public interface ClickListener {
+        void onEntryClick(View view, int entryIndex);
+
+        void onDeleteClick(View view, int entryIndex);
+    }
+
+    private List<Entry> entries;
+    private Context context;
+    private ClickListener clickListener;
+
+    public EntryAdapter(List<Entry> entries) {
+        this.entries = entries;
+    }
+
+    @NonNull
+    @Override
+    public EntryViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        this.context = parent.getContext();
+        LayoutInflater inflater = LayoutInflater.from(this.context);
+
+        View entryRowView = inflater.inflate(R.layout.item_entry, parent, false);
+
+        return new EntryViewHolder(entryRowView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull EntryViewHolder holder, int position) {
+        Entry currentEntry = entries.get(position);
+        holder.getEntryContent().setText(currentEntry.getName());
+
+        // convert long timestamp to actual string representation
+        Date date = new Date(currentEntry.getCreationTime());
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd/MM/yy");
+        holder.getCreationTime().setText(simpleDateFormat.format(date));
+
+        if (currentEntry.isActive()) {
+            holder.getBackground().setBackgroundColor(this.context.getResources().getColor(R.color.active));
+        } else {
+            holder.getBackground().setBackgroundColor(this.context.getResources().getColor(R.color.inactive));
+        }
+
+        switch (currentEntry.getPriority()) {
+            case HIGH: {
+                holder.getPriorityView().setBackgroundColor(this.context.getResources().getColor(R.color.priorityHigh));
+                break;
+            }
+            case NORMAL: {
+                holder.getPriorityView().setBackgroundColor(this.context.getResources().getColor(R.color.priorityMedium));
+                break;
+            }
+            case LOW: {
+                holder.getPriorityView().setBackgroundColor(this.context.getResources().getColor(R.color.priorityLow));
+                break;
+            }
+            default: {
+                // could throw an error or a log entry to show the next developer that we have priority that is not expected
+                break;
+            }
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return this.entries.size();
+    }
+
+    public void setClickListener(ClickListener entryClickListener) {
+        this.clickListener = entryClickListener;
+    }
+
+    public class EntryViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+
+        private TextView entryContent;
+        private TextView creationTime;
+        private View priorityView;
+        private RelativeLayout background;
+        private ImageButton deleteButton;
+
+        public EntryViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            itemView.setOnClickListener(this);
+
+            this.creationTime = itemView.findViewById(R.id.creationTime);
+            this.entryContent = itemView.findViewById(R.id.entryContent);
+            this.priorityView = itemView.findViewById(R.id.priority);
+            this.background = itemView.findViewById(R.id.entryWrapper);
+            this.deleteButton = itemView.findViewById(R.id.delete);
+
+            this.deleteButton.setOnClickListener(this);
+        }
+
+        public TextView getEntryContent() {
+            return this.entryContent;
+        }
+
+        public TextView getCreationTime() {
+            return this.creationTime;
+        }
+
+        public View getPriorityView() {
+            return priorityView;
+        }
+
+        public RelativeLayout getBackground() {
+            return background;
+        }
+
+        @Override
+        public void onClick(View v) {
+            if (clickListener != null) {
+                if (v == this.deleteButton) {
+                    clickListener.onDeleteClick(v, this.getAdapterPosition());
+                } else {
+                    clickListener.onEntryClick(v, this.getAdapterPosition());
+                }
+            }
+        }
+    }
+}
